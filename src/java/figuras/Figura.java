8e52c40c4533a56e079ce348f2b5c1df107169e6
Enrique Tafalla <enrique.tafalla@gmail.es>
2025-12-09 17:32:27 +0100

    re subiendo interfaz Figura.java Close #2

---
+++ src/java/figuras/Figura.java
@@ -0,0 +1,10 @@
+package figuras;
+
+/**
+ * Interfaz Figura: Es una generalización de una figura geométrica.
+ * @author Enrique Tafalla
+ */
+public interface Figura {
+    public double calcularArea();
+    public double calcularPerimetro();
+}
